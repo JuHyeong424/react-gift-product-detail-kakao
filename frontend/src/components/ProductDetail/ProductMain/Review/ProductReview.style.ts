c0aac9532a5e7dd069b889d485386d2694c64c5e
JuHyeong <wkdwngud963@gmail.com>
2025-07-29 22:19:19 +0900

    fix: 스타일 줄바꿈 추가

--- frontend/src/components/ProductDetail/ProductMain/Review/ProductReview.style.ts
+++ frontend/src/components/ProductDetail/ProductMain/Review/ProductReview.style.ts
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 
 export const ProductReviewWrapper = styled.div`
   padding: ${({ theme }) => ` ${theme.spacing.spacing2} ${theme.spacing.spacing3}`};
+  word-break: break-word;
+  overflow-wrap: break-word;
+  white-space: pre-wrap;
 `;
 
 export const ReviewWrapper = styled.div`
